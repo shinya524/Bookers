6e922090c38abf7b7959b8674b4be17e3aee370d
shinya524 <gheena1800@gmail.com>
2022-08-06 08:10:00 +0000

    [Add]3-12 削除機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
 
+
   def index
     @book = Book.new
     @books = Book.all
@@ -14,15 +15,33 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+
+    if @book.save
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def update
+    @book = Book.find(params[:id])
+
+    if  @book.update(book_params)
+      flash[:notice] = 'Book was successfully updated.'
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+
+  def destroy
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    book.destroy
+    redirect_to '/books'
   end
 
  private
